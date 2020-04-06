9d82028a151bb355799d36ef74e6f2e4c52e0c6c
Igor Risteski <pirej30@yahoo.com>
2020-04-06 03:06:31 +0200

    import the component in index.js and render it

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+// import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import HomePage from './components/HomePage';
+
+
+// ReactDOM.render(<HomePage />, document.getElementById("root"));
+render(<HomePage />, document.getElementById("root"));
+// render(<HomePage />)
+
