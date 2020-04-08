5a8c9d6b1aa672d63a60a72a411fc13fca79efa1
Igor Risteski <pirej30@yahoo.com>
2020-04-08 03:41:02 +0200

    To register the key inputs in the input-fields, we need to: 1. Declare change handler for each input 2. Declare state

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Prompt } from "react-router-dom";
 import CourseForm from './CourseForm';
 
 // functional component with arrow function
 const ManageCoursePage = props => {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    function handleTitleChange(event) {
+        // debugger;
+        const updatedCourse = { ...course, title: event.target.value };
+        setCourse(updatedCourse);
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm />
+            <CourseForm course={course} onTitleChange={handleTitleChange} />
         </>
 
     )
