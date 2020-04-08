04cb11b3c077c45851bd41048ed83cae8f1b98f1
Igor Risteski <pirej30@yahoo.com>
2020-04-08 02:36:45 +0200

    Prompt example-- just an example, a temporary one :) It prompts you are you sure you want to leave the ManageCoursePage.. when clicking on another page link in navlinks

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+// import { Prompt } from "react-router-dom";
 
 // functional component with arrow function
 const ManageCoursePage = props => {
     return (
         <>
             <h2>Manage Course</h2>
+            {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
             {props.match.params.slug}
         </>
 
