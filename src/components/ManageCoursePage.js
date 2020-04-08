4ae504d256abeeb871fdcda27e5da7bbfd7efb55
Igor Risteski <pirej30@yahoo.com>
2020-04-09 01:51:17 +0200

    Disable the default refresh on submit, to be able to pass/save the data Save the submitted input (new course) the course is actually saved just temporarily in the db.json, which gets recreated on app start.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 // import { Prompt } from "react-router-dom";
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
 
 // functional component with arrow function
 const ManageCoursePage = props => {
@@ -18,10 +19,15 @@ const ManageCoursePage = props => {
         setCourse(updatedCourse);
     }
 
+    function handleSubmit(event) {
+        event.preventDefault(); // prevent refreshing the input after submit
+        courseApi.saveCourse(course);
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm course={course} onChange={handleChange} />
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} />
         </>
 
     )
